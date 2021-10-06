a595938d60c058dfdebff774d6e851f2432116c1
pajannat <61099858+pajannat@users.noreply.github.com>
2021-10-06 23:25:04 +0900

    Add files via upload

--- アルゴ式/設計技法とデータ構造/グラフ理論入門/Q3-6. 箱の中の箱 (2).py
+++ アルゴ式/設計技法とデータ構造/グラフ理論入門/Q3-6. 箱の中の箱 (2).py
@@ -11,22 +11,34 @@ def main():
     G = [[] for _ in range(N)]
 
     for i in range(N-1):
+        # 頂点A[i]から頂点 i+1 への辺を張る
         G[A[i]].append(i+1)
-    
+
+    # 頂点 0 から各頂点 i までの距離をdist[i]に記録
     dist = [-1] * N
+    # 頂点 0 から頂点 0 までの距離は 0
     dist[0] = 0
 
     d = deque()
     d.append(0)
 
+    # dにキューがある限り処理を続ける
     while d:
+        # dの左端から一つ取り出す
         v = d.popleft()
+        # 頂点 v から到達可能な頂点 i を探索
         for i in G[v]:
+            # 頂点 i に到達済みの場合はスキップ
             if dist[i] != -1:
                 continue
+            # 未到達の場合は、(頂点 0 から)頂点 i までの距離を計算
+            # 頂点 0 から移動するごとに+1する
+            # 累積値が(頂点 0 から)頂点 i までの距離となる
             dist[i] = dist[v] + 1
+            # 頂点 i の先を探索するために、dにキューとしてiを格納
             d.append(i)
 
+    # 答えを出力
     for i in range(1, N):
         print(dist[i])
 
