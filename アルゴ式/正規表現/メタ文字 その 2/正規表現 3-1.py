1e0f3250dd90084539922ddad6f0f410d92b13d3
pajannat <ytchisuga@gmail.com>
2022-04-29 21:39:40 +0900

    Add メタ文字その2

---
+++ アルゴ式/正規表現/メタ文字 その 2/正規表現 3-1.py
@@ -0,0 +1,15 @@
+# 正規表現を取り扱うためのライブラリ
+import re 
+
+# 調べたい文字列 
+S = input().rstrip()  
+# 調べる正規表現 
+reg = r'\d' 
+
+# マッチした文字列情報が格納される (存在しなければ null)  
+search = re.search(reg, S)
+if search:
+    # "Yes" と表示される 
+    print("Yes")
+else:
+    print("No") 
