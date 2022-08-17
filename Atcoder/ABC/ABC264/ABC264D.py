b3e217699fea850a43979d9be1df543b5ad104b1
pajannat <ytchisuga@gmail.com>
2022-08-17 22:01:54 +0900

    modified:   ABC264D.py

--- Atcoder/ABC/ABC264/ABC264D.py
+++ Atcoder/ABC/ABC264/ABC264D.py
@@ -8,10 +8,13 @@ def main():
     # 処理
     ans = 0
 
+    # "atcoder"の先頭から一文字ずつswap回数を計算していく
     for chr in "atcoder":
         for i, s in enumerate(S):
+            #　chrを正しい位置に移動するために必要なswap回数をansに加算
             if s == chr:
                 ans += i
+        # 計算完了した文字は文字列から除去
         S.remove(chr)
     
     # 答えを出力
