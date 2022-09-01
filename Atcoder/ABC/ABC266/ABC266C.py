141ebeb468973061c2dcdabc5d9941c5cf22b70d
pajannat <ytchisuga@gmail.com>
2022-09-02 00:32:19 +0900

    modified:   ABC266C.py

--- Atcoder/ABC/ABC266/ABC266C.py
+++ Atcoder/ABC/ABC266/ABC266C.py
@@ -3,12 +3,24 @@ def main():
     input = stdin.readline
 
     import numpy
-    from itertools import combinations
 
-    def cross_product_ABxAC(ax, ay, bx, by, cx, cy):
+    def cross_product(ax, ay, bx, by, cx, cy):
         vec_AB = [bx-ax, by-ay]
         vec_AC = [cx-ax, cy-ay]
+
         return numpy.cross(vec_AB, vec_AC)
+
+    # 三角形ABCにDが含まれるか判断
+    def solver(A, B, C, D):
+        flg_include = False
+        cross_ABxAD = cross_product(A[0], A[1], B[0], B[1], D[0], D[1])
+        cross_BCxBD = cross_product(B[0], B[1], C[0], C[1], D[0], D[1])
+        cross_CAxCD = cross_product(C[0], C[1], A[0], A[1], D[0], D[1])
+        if cross_ABxAD > 0 and cross_BCxBD > 0 and cross_CAxCD > 0:
+            pass
+        else:
+            flg_include = True
+        return flg_include
 
     # 入力を受け取る
     Ax, Ay = map(int, input().split())
@@ -16,23 +28,26 @@ def main():
     Cx, Cy = map(int, input().split())
     Dx, Dy = map(int, input().split())
 
-    # 処理
-    # ABCDから3点選ぶ組み合わせ comb_ABCD
-    comb_ABCD = combinations([[Ax, Ay], [Bx, By], [Cx, Cy], [Dx, Dy]], 3)
-    for a in comb_ABCD:
-        print(a)
-    
-    # ABCDから1点選ぶ
+    # どの3点を三角形の頂点とし、どの1点の内包を判定するかを示すリスト
+    Input_list = [
+        [[Ax, Ay], [Bx, By], [Cx, Cy], [Dx, Dy]],
+        [[Dx, Dy], [Ax, Ay], [Bx, By], [Cx, Cy]],
+        [[Cx, Cy], [Dx, Dy], [Ax, Ay], [Bx, By]],
+        [[Bx, By], [Cx, Cy], [Dx, Dy], [Ax, Ay]]
+        ]
 
-    # 選んだ1点が他三点からなる三角形に内包されるかを判定
-    # 三角形ABCにDが含まれるか判断するには
-    # vec_AB, vec_BA, vecCA とAD
+    # 処理
+    for lis in Input_list:
+        # 選んだ1点が他三点からなる三角形に内包されるかを判定
+        flg = solver(*lis)
+
+        # flg == False となることがあれば, "No"を出力して終了
+        if flg == False:
+            print("No")
+            exit()
     
-    # 答えを出力
-    print(Ax, Ay)
-    print(Bx, By)
-    print(Cx, Cy)
-    print(Dx, Dy)
+    # flg == False となることがなければ, "Yes"を出力
+    print("Yes")
 
 
 if __name__ == '__main__':
