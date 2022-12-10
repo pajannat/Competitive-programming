01414e1ca1d0e45ee7cf3264d1f61e82d028604e
pajannat <ytchisuga@gmail.com>
2022-12-11 00:54:23 +0900

    modified ABC281B

--- Atcoder/ABC/ABC281/ABC281B.py
+++ Atcoder/ABC/ABC281/ABC281B.py
@@ -5,13 +5,21 @@ def main():
     # 入力を受け取る
     S = input().rstrip()
 
-    flg = True
-    # 処理
-    S_l = S[0]
-    S_r = S[len(S)-1]
-    num = S[1:len(S)-1]
 
-    # 文字列が数字か判定: str.isdigit()
+    # 処理
+    # 文字列の長さを判定
+    if len(S) != 8:
+        print("No")
+        exit()
+
+
+    S_l = S[0]  # 先頭の文字
+    S_r = S[len(S)-1]  # 末尾の文字
+    num = S[1:len(S)-1]  # 中央の文字列
+
+    flg = True
+
+    # 文字列 num が数字か判定: num.isdigit()
     if num.isdigit():
         # 100000 以上 999999 以下の整数か
         if 100000 <= int(num) <= 999999:
@@ -21,7 +29,6 @@ def main():
     else:
         flg = False
 
-    
     # 先頭と末尾が文字か判断
     if S_l.isalpha() and S_r.isalpha():
         pass
