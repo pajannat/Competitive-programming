737a954e004a01a7c48ef31a32aa81171eedf210
pajannat <ytchisuga@gmail.com>
2023-01-11 23:56:27 +0900

    Add A23

---
+++ 競技プログラミングの鉄則_演習問題集/A/A23 - All Free.py
@@ -0,0 +1,21 @@
+def main():
+    from sys import stdin
+    input = stdin.readline
+
+
+    # 入力を受け取る
+    N, M = map(int, input().split())
+    A_m = [ list(map(int, input().split())) for _ in range(M) ]
+
+    # dp = 
+
+    # 処理
+    # 初期化
+    # bitDP
+
+    # 答えを出力
+    print()
+
+
+if __name__ == '__main__':
+    main()
